80907b0f3776e15ac4a8b683f876cc2f5238ee0c
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-12-14 10:41:06 +0100

    Programación

---
+++ src/boletin2_2/Boletin2_3.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_2;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin2_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    /*float lado=3f;
+    /*
+    float area=lado*lado;
+    System.out.println("o area de cadrado é="+area);*/
+   
+    /*float area=(float)Math.pow(3, 2);   
+    System.out.println("o area do cadrado é ="+area);*/
+   
+   double euros = 0;
+   System.out.println("teclear euros :");
+  
+
+   double euroADolar=0;
+   System.out.println("teclear euroADolar :");
+   
+     
+   double dolares=euros*euroADolar;
+    System.out.println(euros+" euros equivalen a"+ dolares +"dolares");
+    
+    
+    
+   
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
